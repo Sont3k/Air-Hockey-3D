66a03efd1c4131d0b851ed97000212c9dc8bc7ab
Markus Neumann <markus.residence@gmail.com>
2023-11-15 00:06:42 +0200

    feat: implement feedback force, custom colliders, score

---
+++ Assets/_App/Scripts/Physics/FeedbackForce.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace _App.Scripts.Physics
+{
+    public class FeedbackForce : MonoBehaviour
+    {
+        [SerializeField] private Rigidbody _rb;
+        [SerializeField] private float _pushForce;
+
+        private void OnCollisionEnter(Collision other)
+        {
+            var otherRigidbody = other.gameObject.GetComponent<Rigidbody>();
+            if (otherRigidbody == null) return;
+            
+            var pushDirection = (transform.position - other.contacts[0].point).normalized;
+            _rb.AddForce(pushDirection * _pushForce, ForceMode.Impulse);
+        }
+    }
+}
--- Assets/_App/Scripts/PlayerStrikerController.cs
+++ Assets/_App/Scripts/PlayerStrikerController.cs
@@ -1,9 +1,19 @@
-﻿using UnityEngine;
+﻿using DG.Tweening;
+using UnityEngine;
 
 namespace _App.Scripts
 {
     public class PlayerStrikerController : MonoBehaviour
     {
+        [Header("Parameters")]
+        [SerializeField] private float _mouseSyncTime;
+        
+        [Header("Borders")]
+        [SerializeField] private Transform _topLimit;
+        [SerializeField] private Transform _bottomLimit;
+        [SerializeField] private Transform _leftLimit;
+        [SerializeField] private Transform _rightLimit;
+
         [Header("References")]
         [SerializeField] private Camera _camera;
         [SerializeField] private Rigidbody _rb;
@@ -12,10 +22,22 @@ namespace _App.Scripts
         {
             if (!Input.GetMouseButton(1)) return;
 
-            if (Physics.Raycast(_camera.transform.position, _camera.transform.forward, out var hitInfo, 5))
+            var ray = _camera.ScreenPointToRay(Input.mousePosition);
+            if (UnityEngine.Physics.Raycast(ray, out var hitInfo))
             {
-                _rb.position = hitInfo.point;
+                UpdateStrikerPosition(hitInfo);
             }
+        }
+
+        private void UpdateStrikerPosition(RaycastHit hitInfo)
+        {
+            if (hitInfo.point.z > _topLimit.position.z) return;
+            if (hitInfo.point.z < _bottomLimit.position.z) return;
+            if (hitInfo.point.x < _leftLimit.position.x) return;
+            if (hitInfo.point.x > _rightLimit.position.x) return;
+
+            var destination = new Vector3(hitInfo.point.x, _rb.position.y, hitInfo.point.z);
+            _rb.DOMove(destination, _mouseSyncTime);
         }
     }
 }
