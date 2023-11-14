f28ceb478ea10995c4b880ea396a7edebe6d4e97
Markus Neumann <markus.residence@gmail.com>
2023-11-14 22:12:30 +0200

    feat: add colliders, creat striker controller

---
+++ Assets/_App/Scripts/PlayerStrikerController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace _App.Scripts
+{
+    public class PlayerStrikerController : MonoBehaviour
+    {
+        [Header("References")]
+        [SerializeField] private Camera _camera;
+        [SerializeField] private Rigidbody _rb;
+
+        private void Update()
+        {
+            if (!Input.GetMouseButton(1)) return;
+
+            if (Physics.Raycast(_camera.transform.position, _camera.transform.forward, out var hitInfo, 5))
+            {
+                _rb.position = hitInfo.point;
+            }
+        }
+    }
+}
---
+++ Assets/_App/Scripts/ScoreController.cs
@@ -0,0 +1,30 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace _App.Scripts
+{
+    public class ScoreController : MonoBehaviour
+    {
+        [SerializeField] private TMP_Text _scoreText;
+
+        private int _playerScore;
+        private int _computerScore;
+
+        public void IncreasePlayerScore()
+        {
+            _playerScore++;
+            UpdateUI();
+        }
+
+        public void IncreaseComputerScore()
+        {
+            _computerScore++;
+            UpdateUI();
+        }
+
+        private void UpdateUI()
+        {
+            _scoreText.text = $"Score: {_playerScore} x {_computerScore}";
+        }
+    }
+}
