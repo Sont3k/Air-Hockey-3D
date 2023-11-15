1ca2de267078fcb92d82f11c605bfd02788c5c0e
Markus Neumann <markus.residence@gmail.com>
2023-11-15 20:22:44 +0200

    feat: implement scoring and fixes

--- Assets/_App/Scripts/ScoreTrigger.cs
+++ Assets/_App/Scripts/ScoreTrigger.cs
@@ -1,4 +1,5 @@
 using System;
+using Cysharp.Threading.Tasks;
 using UnityEngine;
 
 namespace _App.Scripts
@@ -12,11 +13,19 @@ namespace _App.Scripts
     public class ScoreTrigger : MonoBehaviour
     {
         [SerializeField] private ScoreTriggerType _type;
+        
+        private float _delay = 2f;
         public static event Action<ScoreTriggerType> OnScoreTriggeredStatic;
         
         private void OnTriggerEnter(Collider other)
         {
             if (!other.gameObject.CompareTag("Puck")) return;
+            HandleScore().AttachExternalCancellation(destroyCancellationToken);
+        }
+
+        private async UniTask HandleScore()
+        {
+            await UniTask.Delay(TimeSpan.FromSeconds(_delay));
             OnScoreTriggeredStatic?.Invoke(_type);
         }
     }
